eee8d1554b0be4a544a455550509a01005857d55
Krystian Węcławiak <krystianwec652@gmail.com>
2025-06-11 12:21:45 +0200

    Test lintu

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,14 @@ package org.example;
 
 public class Calculator {
 
+    private final int baseValue;
+
+    // Konstruktor z parametrem, który nadaje sens konstruktorowi
+    public Calculator(int baseValue) {
+        this.baseValue = baseValue;
+    }
+
     public int dodaj(final int number1, final int number2) {
-        return number1 + number2;
+        return baseValue + number1 + number2;
     }
 }
