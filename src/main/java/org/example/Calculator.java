59ee1159db3c10afc850fa21a31cfa7f04e8c2e5
Krystian Węcławiak <krystianwec652@gmail.com>
2025-06-11 12:14:07 +0200

    Test lintu

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,20 @@ package org.example;
 
 public class Calculator {
 
+
+
+
+
+
     public int dodaj(final int number1, final int number2) {
         return number1 + number2;
+
+
+
+
+
+
+
+
     }
 }
